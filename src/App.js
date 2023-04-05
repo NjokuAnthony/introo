3bc362afccd4bd87b382cee26c55b97cd4f14057
NjokuAnthony <a_njokuchibuxor@yahoo.com>
2023-04-05 10:08:19 -0700

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './components/navbar/Nav'
+import Carousel from './components/navbar/Carousel'
+
+const App = () => {
+  return (
+    <div>
+
+      <Nav />
+      <Carousel />
+</div>
+  )
+}
+
+export default App
